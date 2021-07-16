9546bcc205e462675ee23134fcef0885de46600e
Liviu-Terebes <tlie3028@scs.ubbcluj.ro>
2021-07-16 12:07:26 +0300

    Improved the generator

--- service/generators/BacktrackingSudokuGenerator.cpp
+++ service/generators/BacktrackingSudokuGenerator.cpp
@@ -5,42 +5,33 @@
 #include "BacktrackingSudokuGenerator.h"
 #include <algorithm>
 #include <random>
+#include <iostream>
 
 SudokuBoard BacktrackingSudokuGenerator::generateBoard(int size) {
     SudokuBoard newBoard(size);
 
-    // First, create a random permutation and use it as the first row and to build the first column
-    std::vector<int> permutation(size * size);
-    for (int i = 0; i < size * size; ++i)
-        permutation[i] = i + 1;
-    std::random_shuffle(permutation.begin(), permutation.end());
+    // Strategy: Fill the diagonal grids with permutations of (1, ... size^2)
+    for(int k = 0; k < size; ++k)
+    {
+        std::vector<int> perm(size * size, 0);
+        for(int i = 1; i <= size * size; ++i)
+            perm[i - 1] = i;
+        std::random_shuffle(perm.begin(), perm.end());
 
-    // First row
-    std::vector<int> firstRow(permutation);
-
-    // Create the first column
-    std::vector<int> firstColumn(size * size);
-    // Method: Copy the first number and the last two numbers from the permutation as the first number on the column
-    // Then, remove them, shuffle the permutation and add the remaining number to the column
-    firstColumn[0] = permutation[0];
-    permutation.erase(permutation.begin());
-    for (int i = 1; i < size; ++i) {
-        firstColumn[i] = permutation[permutation.size() - 1];
-        permutation.pop_back();
+        int startX = k * size;
+        int startY = k * size;
+        int cnt = 0;
+        for(int i = 0; i < size; ++i)
+        {
+            for(int j = 0; j < size; ++j)
+            {
+                newBoard.setCellValue(startX + i, startY + j, perm[cnt++]);
+            }
+        }
     }
-    std::random_shuffle(permutation.begin(), permutation.end());
-    for (int i = size; i < size * size; ++i) {
-        firstColumn[i] = permutation[i - size];
-    }
-
-    // Set the first row and column
-    for (int i = 0; i < size * size; ++i)
-        newBoard.setCellValue(0, i, firstRow[i]);
-    for (int i = 0; i < size * size; ++i)
-        newBoard.setCellValue(i, 0, firstColumn[i]);
 
     // All that's left is to call a backtracking procedure on the remaining cells and return the board
-    backtracking(newBoard, 1, 1);
+    backtracking(newBoard, size, size);
 
     return newBoard;
 }
@@ -52,6 +43,11 @@ bool BacktrackingSudokuGenerator::backtracking(SudokuBoard &board, int row, int 
     int newRow = (column + 1 == board.getSize()) ? row + 1 : row;
     int newColumn = (column + 1 == board.getSize()) ? 1 : column + 1;
     int grid = row / size * size + column / size;
+
+    // Skip assigned cells
+    if(board.getCellValue(row, column) > 0)
+        return backtracking(board, newRow, newColumn);
+
     for (int i = 1; i <= board.getSize(); ++i) {
         // Assign new value to a cell
         board.setCellValue(row, column, i);
